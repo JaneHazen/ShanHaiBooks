86bc8a4feb9038c4c3c9ff818182d12f0b6aaf77
janehazen <iamjane@gmail.com>
2018-08-08 22:24:32 -0700

    cleanup auth

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -5,8 +5,7 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
         console.log(token);
-        const decoded = jwt.verify(token, envVariables.env.JWT_KEY)
-        req.userData = decoded;
+        req.userData = jwt.verify(token, envVariables.env.JWT_KEY);
         next();
     } catch(error){
         return res.status(401).json({
